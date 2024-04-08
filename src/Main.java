badb89b1153c33c5a0add0a980ee7c221c38ffcb
Amrkurrus <amir.kurmanbekov@gmail.com>
2024-04-08 20:01:12 +0500

    Gitignore update, problem 1 bugfix, problem 2 solved.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-
-
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,13 +9,12 @@ public class Main {
                 p1();
                 break;
             case 2:
-                P2();
+                p2();
                 break;
             default:
                 System.out.println("there is no problem: " + tn);
         }
     }
-
     public static void p1() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -29,22 +25,33 @@ public class Main {
         int index = 0;
         int min = 999999;
         System.out.println(p1r(n, nArr, index, min));
-
     }
-
     public static int p1r(int n, int[] arr, int index, int min) {
-        if (index == n - 1) {
-            return min;
-        }
-
         if (arr[index] < min) {
             min = arr[index];
         }
+        if (index == n - 1) {
+            return min;
+        }
         return p1r(n, arr, index + 1, min);
     }
+    public static void p2() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            nArr[i] = sc.nextInt();
+        }
+        int index = 0;
+        int sum = 0;
+        System.out.println(p2r(n, nArr, index, sum));
+    }
 
-
-    public static void P2() {
-        System.out.println("234");
+    public static double p2r(int n, int[] arr, int index, int sum) {
+        sum = sum + arr[index];
+        if (index == n - 1) {
+            return (double) sum / (double) n;
+        }
+        return p2r(n, arr, index + 1, sum);
     }
 }
