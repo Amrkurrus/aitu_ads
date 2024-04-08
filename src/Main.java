667593b58c17c4df6e163cc637ce3e90503d0b8d
Amrkurrus <amir.kurmanbekov@gmail.com>
2024-04-08 20:42:55 +0500

    Problem 5 solved.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,10 +19,14 @@ public class Main {
             case 4:
                 p4();
                 break;
+            case 5:
+                p5();
+                break;
             default:
                 System.out.println("There is no problem: " + tn);
         }
     }
+
     public static void p1() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -33,11 +38,13 @@ public class Main {
         int min = 999999;
         System.out.println(p1r(n, nArr, index, min));
     }
+
     public static int p1r(int n, int[] arr, int index, int min) {
         if (arr[index] < min) min = arr[index];
         if (index == n - 1) return min;
         return p1r(n, arr, index + 1, min);
     }
+
     public static void p2() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -49,29 +56,45 @@ public class Main {
         int sum = 0;
         System.out.println(p2r(n, nArr, index, sum));
     }
+
     public static double p2r(int n, int[] arr, int index, int sum) {
         sum = sum + arr[index];
         if (index == n - 1) return (double) sum / (double) n;
         return p2r(n, arr, index + 1, sum);
     }
+
     public static void p3() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int index = 2;
         System.out.println(p3r(n, index));
     }
-    public static String p3r(int n, int index){
+
+    public static String p3r(int n, int index) {
         if (index == n || n == 1) return "Prime";
         if (n % index == 0) return "Composite";
-        return p3r(n, index+1);
+        return p3r(n, index + 1);
     }
+
     public static void p4() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(p4r(n));
     }
+
     public static int p4r(int n) {
         if (n == 1) return 1;
-        return n * p4r(n-1);
+        return n * p4r(n - 1);
+    }
+
+    public static void p5() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(p5r(n));
+    }
+    public static int p5r(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return p5r(n-1) + p5r(n-2);
     }
 }
