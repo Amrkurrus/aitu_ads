588a5b24e346aecfc7e4f90da1e877671a7edc1e
Amrkurrus <amir.kurmanbekov@gmail.com>
2024-05-24 09:37:43 +0500

    I am Genius

--- src/Main.java
+++ src/Main.java
@@ -40,9 +40,9 @@ public class Main {
         }
         tree.put(mtc2, p2);
         System.out.println(tree.getSize());
-//        Iterable<MyTestingClass> keys = tree.iterator();
-//        for (MyTestingClass k : keys) {
-//            System.out.println(k);
-//        }
+        MyHashTable<MyTestingClass, Person> keys = tree.iterator();
+        keys.printFull();
+
+        System.out.println(tree.get(mtc1));
     }
 }
