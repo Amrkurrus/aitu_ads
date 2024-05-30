98dc0cd1f0ca90411657fa2ba2dd809d3f7a59b0
Amrkurrus <amir.kurmanbekov@gmail.com>
2024-05-30 21:07:51 +0500

    Provide your own implementation for classes:  Vertex, WeightedGraph, Search, BreadthFirstSearch, DijkstraSearch and Main (with example usage) Deleting class Edge

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,8 +1,13 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Vertex<V> {
     private V data;
-    private MyHashTable<Vertex<V>, Double> adjacentVertices;
+    private Map<Vertex<V>, Double> adjacentVertices;
+    public Vertex(V data){
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
     public void addAdjacentVertex(Vertex<V> destination, double weight) {
         adjacentVertices.put(destination, weight);
     }
